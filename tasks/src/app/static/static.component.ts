63a2d25b5f288ed4158afec965d03a6068e77d37
chaitanya <chaitanya.geddam@bentolabs.io>
2025-06-18 01:23:49 +0530

    Static Webpage Task Completed

--- tasks/src/app/static/static.component.ts
+++ tasks/src/app/static/static.component.ts
@@ -6,5 +6,102 @@ import { Component } from '@angular/core';
   styleUrls: ['./static.component.css']
 })
 export class StaticComponent {
-
+  users: any[] = [
+    {
+      profileImage: '',
+      name: 'Robert Whistable',
+      role: 'Product Manager',
+      projects: 24,
+      tasks: 132,
+      insights: 31
+    },
+    {
+      profileImage: '',
+      name: 'Francois Boateng',
+      role: 'Product Manager',
+      projects: 24,
+      tasks: 132,
+      insights: 31
+    },
+    {
+      profileImage: '',
+      name: 'Elliot Bradbury',
+      role: 'Product Manager',
+      projects: 24,
+      tasks: 132,
+      insights: 31
+    },
+    {
+      profileImage: '',
+      name: 'Carlos Heroa',
+      role: 'Product Manager',
+      projects: 24,
+      tasks: 132,
+      insights: 31
+    },
+    {
+      profileImage: '',
+      name: 'Paul Sheratt',
+      role: 'Product Manager',
+      projects: 24,
+      tasks: 132,
+      insights: 31
+    },
+    {
+      profileImage: '',
+      name: 'Sophie Bunn',
+      role: 'Product Manager',
+      projects: 24,
+      tasks: 132,
+      insights: 31
+    },
+    {
+      profileImage: '',
+      name: 'Emili Rose',
+      role: 'Product Manager',
+      projects: 24,
+      tasks: 132,
+      insights: 31
+    },
+    {
+      profileImage: '',
+      name: 'Louise Redknapp',
+      role: 'Product Manager',
+      projects: 24,
+      tasks: 132,
+      insights: 31
+    },
+    {
+      profileImage: '',
+      name: 'Robert Whistable',
+      role: 'Product Manager',
+      projects: 24,
+      tasks: 132,
+      insights: 31
+    },
+    {
+      profileImage: '',
+      name: 'Robert Whistable',
+      role: 'Product Manager',
+      projects: 24,
+      tasks: 132,
+      insights: 31
+    },
+    {
+      profileImage: '',
+      name: 'Robert Whistable',
+      role: 'Product Manager',
+      projects: 24,
+      tasks: 132,
+      insights: 31
+    },
+    {
+      profileImage: '',
+      name: 'Robert Whistable',
+      role: 'Product Manager',
+      projects: 24,
+      tasks: 132,
+      insights: 31
+    },
+  ];
 }
