9cc18985e990c85e28299a3e6f0ad85d835c6398
chaitanya <chaitanya.geddam@bentolabs.io>
2025-06-19 13:43:02 +0530

    Counter application completed

--- tasks/src/app/counter/counter.component.ts
+++ tasks/src/app/counter/counter.component.ts
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./counter.component.css']
 })
 export class CounterComponent {
+  counters: any[] = [];
+  totalCount: number = 0;
 
+  addCounter() {
+    this.counters.push({ count: 0 });
+  }
+
+  resetCounters() {
+    this.counters = [];
+  }
+
+  increment(index: number) {
+    this.counters[index].count++;
+    this.calculateTotalCount();
+  }
+
+  decrement(index: number) {
+    if (this.counters[index].count > 0) {
+      this.counters[index].count--;
+      this.calculateTotalCount();
+    }
+  }
+
+  calculateTotalCount() {
+    this.totalCount = this.counters.reduce((total, counter) => total + counter.count, 0);
+  }
+
+  deleteCounter(index: number) {
+    this.counters.splice(index, 1);
+    this.calculateTotalCount();
+  }
 }
